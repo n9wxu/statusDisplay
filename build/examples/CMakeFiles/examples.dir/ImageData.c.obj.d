9facd8de7de73a4cb18934d73fa7144dd462c7cd
Joseph Julicher <jjulich@amazon.com>
2024-01-16 10:02:26 -0800

    removing the build folder

--- build/examples/CMakeFiles/examples.dir/ImageData.c.obj.d
+++
@@ -1,3 +0,0 @@
-examples/CMakeFiles/examples.dir/ImageData.c.obj: \
- /Users/jjulich/Downloads/RP2040-LCD-1.28/c/examples/ImageData.c \
- /Users/jjulich/Downloads/RP2040-LCD-1.28/c/examples/ImageData.h
